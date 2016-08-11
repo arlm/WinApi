1ebd11177861daf75b1cebfb4894af71e40605bb
Alexandre Rocha Lima e Marcondes <amarcondes@coextant.com>
2016-08-11 12:50:32 +0200

    Staring point for Kernel32 APIs

---
+++ src/WinApi.Kernel32/Kernel32.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) to owners found in https://github.com/arlm/WinApi/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace WinApi.Kernel32
+{
+    public static class Kernel32
+    {
+    }
+}
